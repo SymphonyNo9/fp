d63b9a5fc89d02afd99f743b45779c0c30d1164e
hukai.joseph <hukai.joseph@bytedance.com>
2019-12-25 14:51:01 +0800

    add practice of chapter3
    Change-Id: I2de3b4a2d59f78f6b8c71b3e1cbc9d54b205b66e

---
+++ src/main/scala/fpis/FPIS3List.scala
@@ -0,0 +1,108 @@
+package fpis
+
+sealed trait List[+A] // `List` data type, parameterized on a type, `A`
+case object Nil extends List[Nothing] // A `List` data constructor representing the empty list
+/* Another data constructor, representing nonempty lists. Note that `tail` is another `List[A]`,
+which may be `Nil` or another `Cons`.
+ */
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List{
+  def sum(ints: List[Int]): Int = ints match { // A function that uses pattern matching to add up a list of integers
+    case Nil => 0 // The sum of the empty list is 0.
+    case Cons(x,xs) => x + sum(xs) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
+  }
+
+  def product(ds: List[Double]): Double = ds match {
+    case Nil => 1.0
+    case Cons(0.0, _) => 0.0
+    case Cons(x,xs) => x * product(xs)
+  }
+
+  def apply[A](as: A*): List[A] = // Variadic function syntax
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+
+
+  def tail[A](xs: List[A]) =
+    xs match {
+      case Nil => sys.error("tail of empty list")
+      case Cons(_, t) => t
+    }
+
+  def setHead[A](xs: List[A], nh: A) =
+    xs match {
+      case Nil => sys.error("setHead on empty list")
+      case Cons(_, t) => Cons(nh, t)
+    }
+
+  def drop[A](l: List[A], n: Int): List[A] =
+    if (n <= 0) l
+    else l match {
+      case Nil => Nil
+      case Cons(_, t) => drop(t, n-1)
+    }
+  /*
+  Somewhat overkill, but to illustrate the feature we're using a _pattern guard_, to only match a `Cons` whose head
+  satisfies our predicate, `f`. The syntax is to add `if <cond>` after the pattern, before the `=>`, where `<cond>` can
+  use any of the variables introduced by the pattern.
+  */
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] =
+    l match {
+      case Cons(h,t) if f(h) => dropWhile(t, f)
+      case _ => l
+    }
+
+  def append[A](a1: List[A], a2: List[A]): List[A] =
+    a1 match {
+      case Nil => a2
+      case Cons(h,t) => Cons(h, append(t, a2))
+    }
+
+  /*
+  Note that we're copying the entire list up until the last element. Besides being inefficient, the natural recursive
+  solution will use a stack frame for each element of the list, which can lead to stack overflows for
+  large lists (can you see why?). With lists, it's common to use a temporary, mutable buffer internal to the
+  function (with lazy lists or streams, which we discuss in chapter 5, we don't normally do this). So long as the
+  buffer is allocated internal to the function, the mutation is not observable and RT is preserved.
+  Another common convention is to accumulate the output list in reverse order, then reverse it at the end, which
+  doesn't require even local mutation. We'll write a reverse function later in this chapter.
+  */
+  def init[A](l: List[A]): List[A] =
+    l match {
+      case Nil => sys.error("init of empty list")
+      case Cons(_,Nil) => Nil
+      case Cons(h,t) => Cons(h,init(t))
+    }
+  // 这种实现。。。
+  def init2[A](l: List[A]): List[A] = {
+    import collection.mutable.ListBuffer
+    val buf = new ListBuffer[A]
+    @annotation.tailrec
+    def go(cur: List[A]): List[A] = cur match {
+      case Nil => sys.error("init of empty list")
+      case Cons(_,Nil) => List(buf.toList: _*)
+      case Cons(h,t) => buf += h; go(t)
+    }
+    go(l)
+  }
+
+  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B = // Utility functions
+    as match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+
+  def length[A](as: List[A]): Int =
+    foldRight(as, 0)((_, y) => 1+y)
+
+  @scala.annotation.tailrec
+  def foldLeft[A, B](l: List[A], z:B)(f: (B, A) => B): B =
+    l match {
+      case Nil => z
+      case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+    }
+
+  // todo 后面的练习挺有意思，挺有用的，第一次看没有写出来，过段时间再考一下自己
+}
+
